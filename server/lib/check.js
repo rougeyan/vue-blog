b57c4d2d5992a5c3da977d9737c0302393e67084
Caaalabash <1121062986@qq.com>
2018-05-09 23:14:25 +0800

    Update:自动更新nginx配置文件

--- server/lib/check.js
+++ server/lib/check.js
@@ -1,5 +1,39 @@
-//后端验证数据
+const fs = require('fs')
+const request = require('superagent')
+const path = require('path')
+const filepath = '/alidata/server/nginx-1.4.4/conf/vhosts/blog.conf'
+const filename = path.relative(__dirname,filepath)
+const baseUrl = 'https://blog.calabash.top'
+const fileRegex = /\/static.*?\.(js|css)/g
+
+async function getHtml(){
+  let res = await request.get(baseUrl)
+  if(res){
+    let str = res.text
+    return str.match(fileRegex)
+  }
+}
+
+async function init(){
+  let source = await getHtml()
+  fs.readFile(filename,(err,data)=>{
+    let str = data.toString()
+    let obj = str.match(fileRegex)
+    str = str.replace(fileRegex,function(match){
+      return source[obj.indexOf(match)]
+    })
+    fs.writeFile(filename,str,(err)=>{
+      if(err) return false
+      else{
+        return true
+      }
+    })
+
+  })
+}
+
+module.exports.updateNginxConf = init
+
 module.exports.validateLength = function (value='',min=4,max=16){
   return value.length>=min && value.length<=max
 }
-
--- server/redis/index.js
+++ server/redis/index.js
@@ -1,9 +1,12 @@
 const Redis = require("ioredis");
 const redis = new Redis();
-
+const updateNginx = require('../lib/check').updateNginxConf
 
 redis.on("connect", function () {
   console.log('连接redis成功');
+  updateNginx().then(res=>{
+    console.log('更新结果'+res)
+  })
 });
 
 redis.on("error", function (error) {
